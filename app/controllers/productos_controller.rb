e93308b5804cb036ffece73d11fe2c9d1b8cea0e
kinduff <abarcadabra@gmail.com>
2012-07-10 17:56:14 -0500

    Productos: crea views, acciones de controlador, podemos new, create, edit, update, show

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,4 +1,34 @@
 class ProductosController < ApplicationController
   def index
   end
+
+  def new
+    @producto = Producto.new
+  end
+
+  def create
+    @producto = Producto.new params[:producto]
+    if @producto.save
+      redirect_to edit_producto_path(@producto.id)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @producto = Producto.find params[:id]
+  end
+
+  def update
+    @producto = Producto.find params[:id]
+    if @producto.update_attributes params[:producto]
+      redirect_to edit_producto_path(@producto.id)
+    else
+      render :edit
+    end
+  end
+
+  def show
+    @producto = Producto.find params[:id]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Sephora::Application.routes.draw do
   root :to => "productos#index"
+  resources :productos
 end
