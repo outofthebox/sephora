fada7a573efec445bdef5d1141546cffe2f5f14b
kinduff <abarcadabra@gmail.com>
2012-07-11 12:19:29 -0500

    Producto: Define que campos son accesibles para el usuario

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,5 +1,6 @@
 class Producto < ActiveRecord::Base
   validate :validar
+  attr_accessible :nombre, :precio, :descripcion, :ingredientes, :usos
   def validar
     errors.add :nombre, "Escribe un nombre" unless self.nombre.parameterize.length >= 3
     errors.add :precio, "Escribe un precio" unless self.precio.to_i > 0
