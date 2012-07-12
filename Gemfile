f7289fb434b423f894d48491045dccb53f6e4dd6
kinduff <abarcadabra@gmail.com>
2012-07-12 17:55:43 -0500

    Suscripcion: Agregamos gema y campos ENV

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'pg'
 gem 'haml'
 gem 'jquery-rails'
 gem 'sendgrid'
+gem 'hominid'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- config/initializers/_env.rb
+++ config/initializers/_env.rb
@@ -2,4 +2,6 @@ unless Rails.env.production?
   ENV['GOOGLE_ANALYTICS'] = ''
   ENV['SENDGRID_USERNAME'] = ''
   ENV['SENDGRID_PASSWORD'] = ''
+  ENV['MAILCHIMP_API'] = ''
+  ENV['MAILCHIMP_LIST_ID'] = ''
 end
