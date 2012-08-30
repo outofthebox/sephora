6d0bbc86c84cc1a3e53a4d38d234a36a8c058505
Rafael Soto <fael.soto@gmail.com>
2012-08-30 14:38:13 -0500

    Productos: El listing que aparezcan ordenados por updated_at

--- app/controllers/productos_controller.rb
+++ app/controllers/productos_controller.rb
@@ -1,7 +1,7 @@
 class ProductosController < ApplicationController
 
   def index
-    @productos = Producto.includes(:marca).padres.publicados.page(params[:page]).per(50)
+    @productos = Producto.includes(:marca).padres.publicados.order("updated_at DESC").page(params[:page]).per(50)
   end
 
   def busqueda
