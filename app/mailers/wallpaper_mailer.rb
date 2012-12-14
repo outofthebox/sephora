3664fa61522bd9a48d843dfa9476847bf8cbd5ca
kinduff <abarcadabra@gmail.com>
2012-12-14 12:27:43 -0600

    Wallpapers: Intento de fix 7

--- app/mailers/wallpaper_mailer.rb
+++ app/mailers/wallpaper_mailer.rb
@@ -4,7 +4,7 @@ class WallpaperMailer < ActionMailer::Base
   default from: "sephora@outofthebox.mx", :charset => "UTF-8"
   layout 'mail'
   def enviarmail(imagen, mail)
-    imagen = imagen.replace(/-.*$/, '')
+    imagen = imagen.gsub(/-.*$/, '')
     attachments.inline["#{imagen}.jpg"] = File.read(Rails.root.join("public/wallpapers/#{imagen}.jpg"))
     mail(:to => mail, :subject => "¡Descarga tu wallpaper extraordinario!", :body => ' Tu wallpaper ya está listo para que lo descargues. ¡Disfrútalo y únete a la celebración de una Navidad extraordinaria!')
   end
