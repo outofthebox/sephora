42612cdca00bf57c5283345cfe4bdace99dee70a
Kinduff <abarcadabra@gmail.com>
2013-01-17 16:27:36 -0600

    Obsequios: oculta no publicados

--- app/controllers/paginas_controller.rb
+++ app/controllers/paginas_controller.rb
@@ -27,8 +27,8 @@ class PaginasController < ApplicationController
 
   def obsequios
     cat = [64, 62, 63, 39]
-    @productos = Producto.padres.publicados.rangodeprecios(params[:rango]).where(:categoria_id => cat).order(preciorder(params[:precio])).page(params[:page]).per(perparams(params[:ver]))
-    @productoscount = Producto.padres.publicados.rangodeprecios(params[:rango]).where(:categoria_id => cat).count
+    @productos = Producto.padres.rangodeprecios(params[:rango]).where(:categoria_id => cat, :publicado => true).order(preciorder(params[:precio])).page(params[:page]).per(perparams(params[:ver]))
+    @productoscount = Producto.padres.rangodeprecios(params[:rango]).where(:categoria_id => cat, :publicado => true).count
   end
 
   def suscripcion
