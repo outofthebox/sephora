6681e86e1e7dc3c89883d10edb2a109c0295d65c
Kinduff <abarcadabra@gmail.com>
2013-06-06 16:22:56 -0500

    Mobile: quita popup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
     p = e.descendants.reject{|r| r.parent_id != e.id }
     return p
   end
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
 end
