b754982a6c66e3b07695ee2a47c72b6acaa2f42d
Alejandro Rosales <karnal02@gmail.com>
2013-09-06 16:22:39 -0500

    Busqueda y carrusel de producto

--- app/assets/javascripts/mobile.js.coffee
+++ app/assets/javascripts/mobile.js.coffee
@@ -1,8 +1,22 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+# = require jquery
+# = require jquery_ujs
+# = require responsiveslides.min.js
+
 $("#suscripcion_sus").click ->
   if $(this).is(":checked")
     $("#send").removeAttr "disabled"
   else
     $("#send").attr "disabled", "disabled"
+$ ->
+  $("#slider4").responsiveSlides
+    auto: true
+    timeout: 10000
+    pager: false
+    nav: true
+    speed: 500
+    namespace: "callbacks"
+    before: ->
+      $(".events").append "<li>before event fired.</li>"
+
+    after: ->
+      $(".events").append "<li>after event fired.</li>"
--- app/controllers/mobile_controller.rb
+++ app/controllers/mobile_controller.rb
@@ -1,4 +1,39 @@
 class MobileController < ApplicationController
   def home
   end
+
+    def mobilbusqueda
+    params[:buscar][:q] = params[:buscar][:q].gsub(/\//, " ")
+    @marcas_seleccionadas = params[:marca].split(",").map{|m| m.to_i } unless params[:marca].nil?
+    base = Producto.search(params[:buscar][:q], :with => {:publicado => true, :parent_id => 0})
+    if @marcas_seleccionadas.nil? || @marcas_seleccionadas.empty? 
+      @productos_filtrados = base
+    else
+      @productos_filtrados = Producto.search(params[:buscar][:q], :with => {:publicado => true, :parent_id => 0, :marca_id => @marcas_seleccionadas})
+    end
+    if params[:ordenar] == "marca"
+      @productos_filtrados = Producto.search(params[:buscar][:q], :with => {:publicado => true, :parent_id => 0}, :order => 'marca ASC')
+    end
+    if params[:precio]
+      arrange = params[:precio]
+      if arrange == 'alto'
+        @productos_filtrados = Producto.search(params[:buscar][:q], :with => {:publicado => true, :parent_id => 0}, :order => 'precio DESC')
+      elsif arrange == 'bajo'
+        @productos_filtrados = Producto.search(params[:buscar][:q], :with => {:publicado => true, :parent_id => 0}, :order => 'precio ASC')
+      end
+    end
+    @productos = @productos_filtrados.page(params[:page]).per(perparams(params[:ver]))
+    @count = @productos_filtrados.count
+    a = []
+    base.each do |r|
+      a << r.marca
+    end
+    @marcas_para_categoria = a.compact.uniq.sort
+  end
+  
+  def mobileproducto
+    @producto = Producto.includes(:marca, :presentaciones).publicados.where(:slug => params[:slug]).first
+    @categoria = Categoria.find(@producto.categoria_id)
+    @productos_relacionados = @categoria.productos.padres.publicados.where("productos.id != ?", @producto.id).sample(3)
+  end
 end
