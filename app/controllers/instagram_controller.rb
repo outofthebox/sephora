6c31a1b2084c2d71b6e813c39aeb03f809ae8012
Gess Gallardo <gessgallardo@iMac-de-Gess.local>
2013-11-14 16:35:57 -0600

    new stuff

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -4,7 +4,7 @@ class InstagramController < ApplicationController
   end
 
   def new_stuff
-  	puts request.body.read
+  	render :text => request.body.read
     # Instagram.process_subscription(request.body.read) do |handler|
     #   handler.on_tag_changed do |tag, data|
     #     Instagram.tag_recent_media(tag).data.each do |data|
