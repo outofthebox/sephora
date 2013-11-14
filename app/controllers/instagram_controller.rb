5ada59659c678446036583fb982154d42dc70319
Gess Gallardo <gessgallardo@iMac-de-Gess.local>
2013-11-14 16:24:19 -0600

    prueba
    seguimos probando :(!

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -4,24 +4,25 @@ class InstagramController < ApplicationController
   end
 
   def new_stuff
-    Instagram.process_subscription(request.body.read) do |handler|
-      handler.on_tag_changed do |tag, data|
-        Instagram.tag_recent_media(tag).data.each do |data|
-          puts data.inspect
-          sephoragram = Sephoragram.find_or_create_by_instagram_id(
-            :instagram_id => data.id,
-            :instagram_link => data.link,
-            :pic_thumb => data.images.thumbnail.url,
-            :pic_med => data.images.low_resolution.url,
-            :pic_large => data.images.standard_resolution.url,
-            :fullname => data.user.full_name,
-            :username => data.user.username
-          )
-          sephoragram.save
-        end
-      end
-    end
-    render :text => ""
+  	puts request.body.read
+    # Instagram.process_subscription(request.body.read) do |handler|
+    #   handler.on_tag_changed do |tag, data|
+    #     Instagram.tag_recent_media(tag).data.each do |data|
+    #       puts data.inspect
+    #       sephoragram = Sephoragram.find_or_create_by_instagram_id(
+    #         :instagram_id => data.id,
+    #         :instagram_link => data.link,
+    #         :pic_thumb => data.images.thumbnail.url,
+    #         :pic_med => data.images.low_resolution.url,
+    #         :pic_large => data.images.standard_resolution.url,
+    #         :fullname => data.user.full_name,
+    #         :username => data.user.username
+    #       )
+    #       sephoragram.save
+    #     end
+    #   end
+    # end
+    # render :text => ""
   end
 
   def suscribir
