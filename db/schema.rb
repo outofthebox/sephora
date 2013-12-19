7ed53d33aa900f14928243be3d98083e0f02883d
Alejandro <karnal02@gmail.com>
2013-12-19 11:14:08 -0600

    Blog, new y editar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131218215121) do
+ActiveRecord::Schema.define(:version => 20131219162359) do
 
   create_table "blog_categorias", :force => true do |t|
     t.string   "categoria"
@@ -228,6 +228,13 @@ ActiveRecord::Schema.define(:version => 20131218215121) do
     t.integer  "raiting"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "redactor_assets", :force => true do |t|
+    t.string   "asset_file_name"
+    t.string   "asset_content_type"
+    t.integer  "asset_file_size"
+    t.datetime "asset_updated_at"
   end
 
   create_table "registros", :force => true do |t|
