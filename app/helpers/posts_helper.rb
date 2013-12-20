bd86bc77033af75ce8a99b64f85e3f111d5c7bae
Gess Gallardo <gessgallardo@iMac-de-Gess.local>
2013-12-20 12:02:07 -0600

    CSS Blog Sephora

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
+require 'ostruct'
+
 module PostsHelper
+	def stats(id)
+		le_post = Post.find id
+    le_categorias = BlogCategoria.where(:id => le_post.categoria_id)
+    le_comments = Comment.where(:publicado => true, :post_id => id).order('created_at DESC')
+    json = {:post => le_post, :categorias => le_categorias, :comments => le_comments}
+    
+    stats = OpenStruct.new(json)
+    return stats
+	end
 end
