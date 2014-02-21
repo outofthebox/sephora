63ad4bd4aed55f9050bfd11f51dca73d1b8c3f18
Gess Gallardo <gessgallardo@iMac-de-Gess.local>
2014-02-21 10:50:08 -0600

    se agrega propiedad de visto a productos para conocer visitas

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131219162359) do
+ActiveRecord::Schema.define(:version => 20140221163803) do
 
   create_table "blog_categorias", :force => true do |t|
     t.string   "categoria"
@@ -154,6 +154,25 @@ ActiveRecord::Schema.define(:version => 20131219162359) do
   add_index "mobileusers", ["email"], :name => "index_mobileusers_on_email", :unique => true
   add_index "mobileusers", ["reset_password_token"], :name => "index_mobileusers_on_reset_password_token", :unique => true
 
+  create_table "models", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.string   "mobilelogin"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "models", ["email"], :name => "index_models_on_email", :unique => true
+  add_index "models", ["reset_password_token"], :name => "index_models_on_reset_password_token", :unique => true
+
   create_table "post_favs", :force => true do |t|
     t.integer  "usuario_id"
     t.integer  "post_id"
@@ -214,6 +233,7 @@ ActiveRecord::Schema.define(:version => 20131219162359) do
     t.string   "upc"
     t.string   "image_code"
     t.string   "personalidad"
+    t.integer  "visto"
   end
 
   add_index "productos", ["categoria_id"], :name => "index_productos_on_categoria_id"
