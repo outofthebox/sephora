27cb25016aa23495e7978b36b6108ee641b7ddff
Gess Gallardo <hola@gessgallardo.com>
2014-06-30 02:37:03 -0500

    sephora-collection

--- app/controllers/sephora_collection_controller.rb
+++ app/controllers/sephora_collection_controller.rb
@@ -1,11 +1,14 @@
 class SephoraCollectionController < ApplicationController
+  layout "sephora_collection"
   before_filter :set_bg_white, :only => :maquillaje
   before_filter :set_bg_black, :only => :index
+  before_filter :get_productos, :only => :interna
+  before_filter :get_collection, :only => :interna
 
   def index
   end
 
-  def maquillaje
+  def internas
   end
 
   def outrageus_land
@@ -25,5 +28,29 @@ class SephoraCollectionController < ApplicationController
 
   def set_bg_black
     @background = "black"
+  end
+
+  def get_productos
+    case params[:interna]
+    when "maquillaje"
+      productos = Producto.where(:upc => ["3378872072713","3378872079101","3378872059899","3378872073192","3378872068556","3378872078715","1332048","3378872063827","3378872080572"]);
+      @productos = productos.sample(4);
+    when "skincare"
+      productos =  Producto.where(:upc => ["3378872062776","3378872055440","3378872066095","1258383","3378872077558","1258342","1258334","3378872066156","3378872051077"])
+      @productos = productos.sample(4);
+    when "accesorios"
+      productos = Producto.where(:upc => ["1169846","1255850","1356492","1253335","400013774804","3378872027935","3378872047599","1032911","3378872070115"])
+      @productos = productos.sample(4)
+    when "bath"
+      productos = Producto.where(:upc => ["3378872060857", "3378872079736", "3378872065517", "3378872061410", "3378872061557", "3378872079781", "3378872060710", "3378872061694", "3378872066170"])
+      @productos = productos.sample(4)
+    end
+  end
+
+  def get_collection
+    interna = params[:interna]
+    if ["maquillaje", "skincare", "accesorios", "bath"].include?(interna)
+      @collection = interna
+    end
   end
 end
--- app/helpers/sephora_collection_helper.rb
+++ app/helpers/sephora_collection_helper.rb
@@ -1,3 +1,30 @@
-module SephoraCollectionHelper
+#!/bin/env ruby
+# encoding: utf-8
 
+module SephoraCollectionHelper
+  def get_musts
+    musts = [
+      'Labios camaleónicos. 12 tonos para elegir. Rouge Infusion Lip Ink $225',
+      '¿Look natural, glamoroso o sofisticado? ¡Tú decides! Rouge Infusion Lip Ink $225',
+      'Crea pestañas glamorosas, extravagantes, con ultra volumen, en un solo paso. Outrageous Volume Mascara. $245',
+      'Olvídate de esperar años para que sequen tus uñas, Color Hit te da secado rápido y 5 días de brillo. $90',
+      'Lleva en tus manos desde nudes hasta neones, ¡tienes 49 opciones para elegir! Color Hit. $90',
+      '60 tonos y 4 efectos diferentes para looks ilimitados ¿Lista para revelar tu personalidad? Colorful Eyeshadows. $200',
+      'Intensifica tu mirada con delineados a color ¡24 Horas a prueba de todo! Colorful Waterproof Eyeliner 24 HR Wear. $175',
+      '¡Aprende a maquillarte como una PRO! Maquillaje y tutos para crear un look totalmente irresistible. Pro Lesson Palette. $475',
+      'Matices brillantes, destellantes y metálicos, el toque final que tu sonrisa necesita. Rouge Shine Lipstick. $200',
+      'No necesitas ir a Brasil para lograr un bronceado de envidia en tu rostro. Sol de Río Bronzer. $350'
+    ]
+    musts.sample
+  end
+
+  def get_tips
+    tips = ["Si tu piel es seca usa maquillajes líquidos y si es grasa opta por maquillajes en polvo. #SephoraTrick","Para encontrar tu tono correcto de base, pruébala sobre tu mandíbula, si se difumina por completo en tu piel, encontraste tu tono ideal. #SephoraTrick","Aplica una capa de lip stain antes de tu lipstick, así, si éste se desvanece, aún tendrás color en tus labios. #SephoraTrick","Al aplicar tu mascara enfócate en el centro de las pestañas superiores, así agrandarás tu mirada. #SephoraTrick","Si quieres pestañas con mayor volumen busca una mascara con un cepillo grueso, y si lo que buscas es rizarlas busca uno curvo. #SephoraTrick","Antes de aplicar tu esmalte limpia tu uña con removedor de esmalte, así limpiarás la superficie y la aplicación será mejor. #SephoraTrick","Consigue un color más intenso humedeciendo la brocha al aplicar tus sombras en polvo. #SephoraTrick","Si tienes una sombra tono vainilla puedes usarla para iluminar tu mirada, aplícala debajo de la ceja y al lado del lagrimal. #SephoraTrick","Para que tu delineador dure por más tiempo, aplica antes un poco de sombra del mismo tono, así se fijará y el color será más intenso. #SephoraTrick","¡Atrévete a romper las reglas! Lleva un doble delineado en tonos contrastantes para darle un twist a tu look. #SephoraTrick","Para que tu lipstick dure por más tiempo aplícalo por capas: Aplica una capa, besa un pañuelo y repite el paso. #SephoraTrick","Los tonos de lipstick de la temporada son anaranjado y rosa, si te atreves a llevarlos mantén el resto de tu maquillaje sutil. #SephoraTrick","Si quieres lograr unos labios con mayor volumen delinéalos por fuera con el delineador universal de Sephora Collection. #SephoraTrick"]
+    tips.sample
+  end
+
+  def get_texture
+    index = [1,2,3,4,5,6,7,8,9].sample
+    "sephora_collection/texturas/maquillaje/m_textura#{index}.jpg"
+  end
 end
