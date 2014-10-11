4c25f1ca3b71e66898bf72582d3327bec20f4841
Gess Gallardo <hola@gessgallardo.com>
2014-10-11 14:24:16 -0500

    cache y fiesta

--- app/helpers/aniversario_catorce_helper.rb
+++ app/helpers/aniversario_catorce_helper.rb
@@ -1,7 +1,11 @@
 module AniversarioCatorceHelper
 	def restricted date, link
 		date = date.split(",")
-		fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,date[3].to_i,date[4].to_i,date[5].to_i)
+		if date[3] == "17"
+			fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,17,date[4].to_i,date[5].to_i, "-05:00")
+		else
+			fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,11,date[4].to_i,date[5].to_i, "-05:00")
+		end
 		unless fecha <= DateTime.current()
 			"#"
 		else
@@ -10,8 +14,13 @@ module AniversarioCatorceHelper
 	end
 
 	def coming_soon?(date)
+		fecha = nil
 		date = date.split(",")
-		fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,date[3].to_i,date[4].to_i,date[5].to_i)
+		if date[3] == "17"
+			fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,17,date[4].to_i,date[5].to_i, "-05:00")
+		else
+			fecha = DateTime.new(date[0].to_i,date[1].to_i,date[2].to_i,11,date[4].to_i,date[5].to_i, "-05:00")
+		end
 		DateTime.current() < fecha 
 	end
 end
