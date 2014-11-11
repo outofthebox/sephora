85815f2634257cc8fd695997e6fcae7e20313aec
Gess Gallardo <hola@gessgallardo.com>
2014-11-11 02:45:40 -0600

    sitemap for sephora

--- app/controllers/sitemap_controller.rb
+++ app/controllers/sitemap_controller.rb
@@ -4,9 +4,13 @@ class SitemapController < ApplicationController
   def index
     headers['Content-Type'] = 'application/xml'
     last_post = Post.last
-    if stale?(:etag => last_post, :last_modified => last_post.updated_at.utc)
+    last_product = Producto.last
+    if stale?(:etag => last_post, :last_modified => last_post.updated_at.utc) || stale?(:etag => last_product, :last_modified => last_product.updated_at.utc)
       respond_to do |format|
-        format.xml { @posts = Post.all }
+        format.xml {
+          @posts = Post.all
+          @products = Producto.all 
+        }
       end
     end
   end
