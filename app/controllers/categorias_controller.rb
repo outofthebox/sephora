685609b1522139d32fd1e82e5eb28bc63c702893
Gess Gallardo <hola@gessgallardo.com>
2014-12-18 16:38:13 -0600

    fixes categorias

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -6,10 +6,10 @@ class CategoriasController < ApplicationController
   def ver
     @marcas_seleccionadas = params[:marca].split(",").map{|m| m.to_i } unless params[:marca].nil?
     @categoria = Categoria.includes(:productos).by_slug(params[:categoria]).first
-    subcategorias = @categoria.descendants
+    subcategorias = @categoria.descendants rescue []
     @subcategorias = subcategorias.reject{|r| r.parent_id != @categoria.id }
     # para encontrar productos en categorías descendientes
-    catmap = [@categoria.id] + @subcategorias.map{|s| s.id }
+    catmap = [@categoria.id] + @subcategorias.map{|s| s.id } rescue []
     @marcas_para_categoria = Categoria.encontrar_marcas(catmap)
     if params[:marca].blank?
       @productos = Producto.joins(:marca).publicados.padres.where(:categoria_id => catmap).order(("marcas.marca ASC"  if params[:ordenar] == "marca")).order(preciorder(params[:precio])).order('foto_file_name ASC').page(params[:page]).per(perparams(params[:ver]))
