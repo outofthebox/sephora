9d59a46ab8ae898b0ba9e1043ff198e8c64f4cfc
Gess Gallardo <hola@gessgallardo.com>
2015-01-11 19:34:15 -0600

    events admin

--- app/controllers/administracion/events_controller.rb
+++ app/controllers/administracion/events_controller.rb
@@ -10,19 +10,21 @@ class Administracion::EventsController < ApplicationController
 
 	def create
 		event = Event.new(event_params)
-    tiendas_params.each do |tp|
-    	if tp && tp[:activo] == "on"
-    		tienda_id = tp[:tienda_id]
-    		link = tp[:link]
-    		date = tp[:date].inspect
-	    	she = StoreHasEvent.new(:tienda_id=> tienda_id, :link => link, :dates => date, :event_id => event.id)
-	    	if she.save
-	    		event.store_has_events << she
-	    	end
+		if params[:tienda]
+	    tiendas_params.each do |tp|
+	    	if tp && tp[:activo] == "on"
+	    		tienda_id = tp[:tienda_id]
+	    		link = tp[:link]
+	    		date = tp[:date].inspect
+		    	she = StoreHasEvent.new(:tienda_id=> tienda_id, :link => link, :dates => date, :event_id => event.id)
+		    	if she.save
+		    		event.store_has_events << she
+		    	end
+		    end
 	    end
-    end
+	  end
     if event.save
-    	redirect_to admin_event_path
+    	redirect_to admin_events_path
     end
 	end
 
@@ -34,6 +36,12 @@ class Administracion::EventsController < ApplicationController
     event = Event.find(params[:id])
     event.update_attributes(event_params)
     redirect_to admin_event_path
+  end
+
+  def destroy
+  	event = Event.find(params[:id])
+  	event.destroy
+  	redirect_to admin_events_path
   end
 
 	private
