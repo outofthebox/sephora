b1c80e9552feda5e4ac01d337bad3dc304f4407f
kinduff <abarcadabra@gmail.com>
2015-02-03 20:49:00 -0300

    Instagram API keys

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,6 +1,6 @@
 require "instagram"
 
 Instagram.configure do |config|
-	config.client_id =	ENV['insta_client_id'];
-	config.client_secret = ENV['insta_client_secret'];
+	config.client_id =	"bd7b8d3c8a0d4c18b67fe0b28c0d6bec";
+	config.client_secret = "2d40bf5a9875403a985bcd762251cfd1";
 end
