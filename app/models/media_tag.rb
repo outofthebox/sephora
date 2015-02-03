3a0072248d2d592cfc2a4f0bbc850c12fb5d1336
kinduff <abarcadabra@gmail.com>
2015-02-03 17:38:55 -0300

    Adds controller fetcher and suscribe actions for instagram suscriptions. Adds parse method for mediatag

--- app/models/media_tag.rb
+++ app/models/media_tag.rb
@@ -1,3 +1,17 @@
 class MediaTag < ActiveRecord::Base
   attr_accessible :instagram_id, :instagram_link, :thumb_url, :pic_url, :video_url, :fullname, :username, :approved
+
+  def self.parse data
+    attributes = {
+      :instagram_id => data.id,
+      :instagram_link => data.link,
+      :thumb_url => data.images.low_resolution.url,
+      :pic_url => data.images.standard_resolution.url,
+      :video_url => data.videos.try(:standard_resolution).try(:url),
+      :fullname => data.user.full_name,
+      :username => data.user.username
+    }
+    media = MediaTag.find_by_instagram_id(attributes[:instagram_id])
+    MediaTag.create!(attributes) if media.nil?
+  end
 end
