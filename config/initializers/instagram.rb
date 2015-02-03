740b865c0dd60c58b1e8fe457da15b2789314784
kinduff <abarcadabra@gmail.com>
2015-02-03 20:20:53 -0300

    Instagram API keys to env vars

--- config/initializers/instagram.rb
+++ config/initializers/instagram.rb
@@ -1,7 +1,6 @@
 require "instagram"
 
 Instagram.configure do |config|
-	config.client_id =	"d95fe733b2454933aeeaf042b1ab1d44";
-	config.client_secret = "5bffb51b30124478a1a5a5828967d741";
-	config.access_token = "10616307.d95fe73.8e643906b02b4c6a8d6a12be02a744ab";
+	config.client_id =	ENV['insta_client_id'];
+	config.client_secret = ENV['insta_client_secret'];
 end
