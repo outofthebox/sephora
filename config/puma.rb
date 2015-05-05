a8aa42c7ece141ccc4ef65525dd9894d720848cf
kinduff <abarcadabra@gmail.com>
2015-05-05 12:25:42 -0500

    Puma forever

---
+++ config/puma.rb
@@ -0,0 +1,20 @@
+workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads threads_count, threads_count
+
+preload_app!
+
+rackup      DefaultRackup
+port        ENV['PORT']     || 3000
+environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  ActiveSupport.on_load(:active_record) do
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+             Rails.application.config.database_configuration[Rails.env]
+    config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
+    config['pool']            =   ENV['DB_POOL'] || 20
+    ActiveRecord::Base.establish_connection(config)
+  end
+end
+
