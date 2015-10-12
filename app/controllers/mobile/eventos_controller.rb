3431bb910e637c7d7f07b79605bddfb8d6f01288
Gess Gallardo <hola@gessgallardo.com>
2015-10-12 16:24:29 -0500

    evento nuevo acomodo

--- app/controllers/mobile/eventos_controller.rb
+++ app/controllers/mobile/eventos_controller.rb
@@ -1,6 +1,6 @@
 class Mobile::EventosController < MobileController
 	def index
-    m = [78, 75, 92, 79, 77, 76, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91]
+    m = [78, 93, 75, 92, 79, 77, 76, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91]
     @eventos = Event.all.index_by(&:id).values_at(*m) rescue []
     @tiendas = Tienda.all
   end
