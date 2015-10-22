6a44f127d97626c228b793ec100e0aa6344e5d8c
Gess Gallardo <hola@gessgallardo.com>
2015-10-22 15:57:29 -0500

    eventos

--- app/controllers/mobile/eventos_controller.rb
+++ app/controllers/mobile/eventos_controller.rb
@@ -1,8 +1,9 @@
 class Mobile::EventosController < MobileController
 	def index
-    m = [78, 93, 75, 92, 79, 77, 76, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91]
-    n = m & Event.where({id: m}).pluck(:id)
-    @eventos = Event.where({id: n}).index_by(&:id).values_at(*n) rescue []
+    #m = [78, 93, 75, 92, 79, 77, 76, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91]
+    #n = m & Event.where({id: m}).pluck(:id)
+    #@eventos = Event.where({id: n}).index_by(&:id).values_at(*n) rescue []
+    @eventos = Event.all
     @tiendas = Tienda.all
   end
 
