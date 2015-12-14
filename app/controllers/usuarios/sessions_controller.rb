66fe2accc7cecfad197b09f551dd7295eb470730
Gess Gallardo <hola@gessgallardo.com>
2015-12-14 12:10:35 -0600

    fix login

--- app/controllers/usuarios/sessions_controller.rb
+++ app/controllers/usuarios/sessions_controller.rb
@@ -1,20 +1,3 @@
 class Usuarios::SessionsController < Devise::SessionsController
-	#layout :select_layout_type
-  # def new
-  #   super
-  # end
-
-  # def create
-  #   super
-  # end
-
-  # POST /resource/sign_in
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    set_flash_message(:notice, :signed_in) if is_flashing_format?
-    raise resource.inspect
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
+	layout :select_layout_type
 end
