d724dee806c15d4fed52d2313f861c19765982c9
Gess Gallardo <hola@gessgallardo.com>
2016-10-03 23:59:07 -0500

    impossible to achieve mockup, full img landing

--- app/controllers/beautyfair_controller.rb
+++ app/controllers/beautyfair_controller.rb
@@ -1,6 +1,16 @@
 class BeautyfairController < ApplicationController
-  layout 'beautyfair'
+  layout :select_beauty_layout
+  
   def home; end
   def tour; end
   def playa; end
+
+  def select_beauty_layout
+    case action_name
+    when "antara"
+      return 'beautyfair_2'
+    else
+      return 'beautyfair'
+    end
+  end
 end
